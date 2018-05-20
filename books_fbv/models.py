27bcfeab0352319bc335874069b6319cd8f25fb5
Rayed Alrashed <rrashed@wirefilter.com>
2018-05-20 14:03:45 +0300

    Change __unicode__ to __str__

--- books_fbv/models.py
+++ books_fbv/models.py
@@ -6,7 +6,7 @@ class Book(models.Model):
     name = models.CharField(max_length=200)
     pages = models.IntegerField()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
     def get_absolute_url(self):
